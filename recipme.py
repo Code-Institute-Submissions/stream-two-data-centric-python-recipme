3a75433ba7ad41421d0c62d08f48b4c9f0629b29
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-05-03 12:56:41 +0100

    split off read requests to own file

--- recipme.py
+++ recipme.py
@@ -1,7 +1,7 @@
 import os
 import pymysql
 import unittest
-import service
+import db_read
 from myenviron import ROOT_USERNAME, ROOT_PASSWORD, REMOTE_USER, REMOTE_PASSWORD, REMOTE_HOST, DATABASE_NAME
   
 #### TO COME FROM FORM ####
@@ -17,7 +17,7 @@ ingredient = 'Butter'
 
 def get_user_id(user_values):
     """ GET A GIVEN USERS ID BASED ON LOGIN VALUES """
-    query = service.query_read_recipes()
+    query = db_read.query_read_recipes()
     user = query.query_user_id(user_values)
     user_id = [item['UserId'] for item in user]
     #print(user_id)
@@ -25,7 +25,7 @@ def get_user_id(user_values):
     
 def get_all_mini_recipes(search_by, search_value, order_by, direction):
     """ GET MINI RECIPES FOR DISPLAY ON PUBLIC FEED """ 
-    query_recipe = service.query_read_recipes()
+    query_recipe = db_read.query_read_recipes()
     recipe = query_recipe.query_all_mini_recipes(search_by, search_value, order_by, direction)
     print(recipe)
     return recipe
@@ -33,7 +33,7 @@ def get_all_mini_recipes(search_by, search_value, order_by, direction):
 def get_mini_user_recipes(user_values, search_by, order_by, direction):
     """ GET MINI RECIPES FOR DISPLAY ON USERS OWN FEED ONLY """
     user_id = get_user_id(user_values)
-    query_recipe = service.query_read_recipes()
+    query_recipe = db_read.query_read_recipes()
     recipe = query_recipe.query_all_mini_recipes(search_by, user_id[0], order_by, direction)
     print(recipe)
     return recipe
@@ -41,35 +41,35 @@ def get_mini_user_recipes(user_values, search_by, order_by, direction):
 
 def get_ingredients_for_full_recipe(recipe_id):
     """ GETS ALL THE INGREDIENTS FOR DISPLAY ON THE FULL RECIPE PAGE"""
-    query_recipe = service.query_read_recipes()
+    query_recipe = db_read.query_read_recipes()
     ingredients = query_recipe.query_ingredients_for_full_recipe(recipe_id)
     print(ingredients)
     return ingredients
 
 def get_method_for_full_recipe(recipe_id):
     """ GETS THE METHOD FOR DISPLAY ON THE FULL RECIPE PAGE """
-    query_recipe = service.query_read_recipes()
+    query_recipe = db_read.query_read_recipes()
     method = query_recipe.query_method_for_full_recipe(recipe_id)
     print(method)
     return method
 
 def get_filtered_mini_recipes(search_by, search_value, course, cuisine, order_by, direction):
     """ GETS A FILTERED SET OF RECIPES SET BY CUISINE AND COURSE """
-    query_recipe = service.query_read_recipes()
+    query_recipe = db_read.query_read_recipes()
     filtered_recipes = query_recipe.query_filter_mini_recipes(search_by, search_value, course, cuisine, order_by, direction)
     print(filtered_recipes)
     return filtered_recipes
 
 def get_recipes_by_ingredient(search_by, search_value, ingredient, order_by, direction):
     """ GETS A SET OF RECIPES FILTERED BY AN INGREDIENTS SEARCH """
-    query_recipe = service.query_read_recipes()
+    query_recipe = db_read.query_read_recipes()
     search_recipe = query_recipe.query_search_ingredient(search_by, search_value, ingredient, order_by, direction)
     print(search_recipe)
     return search_recipe
 
 def get_saved_recipes_for_user(user_id, order_by, direction):
     """ GET ALL THE SAVED RECIPES FOR A GIVEN USER """
-    query_recipe = service.query_read_recipes()
+    query_recipe = db_read.query_read_recipes()
     saved_recipes = query_recipe.query_users_saved_recipes(user_id, order_by, direction)
     print(saved_recipes)
     return saved_recipes
--- test_recipme.py
+++ test_recipme.py
@@ -1,14 +1,14 @@
 import os
 import pymysql
 import recipme
-import service
+import db_read
 import unittest
 from myenviron import ROOT_USERNAME, ROOT_PASSWORD, REMOTE_USER, REMOTE_PASSWORD, REMOTE_HOST, DATABASE_NAME
 
 class TestRecipme(unittest.TestCase):
     def test_read_all_from_one_table(self):
         table = "recipe"
-        query = service.read_one_table(table)
+        query = db_read.read_one_table(table)
         result = query.read_all_from_one_table()
 
         self.assertEqual(type(result), list)
@@ -104,5 +104,5 @@ class TestRecipme(unittest.TestCase):
         # USERNAME FROM GIVEN ID
 
         self.assertEqual(type(recipe), list)
-        self.assertEqual(author, 'darchard')  ORIGINAL AUTHOR
+        self.assertEqual(author, 'darchard')  
         
