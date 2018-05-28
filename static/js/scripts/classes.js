aa2aeed4f7839a6312088f1edb552808f419fe48
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-05-28 17:07:51 +0100

    refactor js and scss to be more modular

---
+++ static/js/scripts/classes.js
@@ -0,0 +1,232 @@
+
+// -------------------------------- VISUALISATION ---------------------------- //
+
+
+class Charts {
+
+    constructor(recipeData) {
+
+        this.recipeData = recipeData;
+    }
+
+    makePieGraph() {
+
+        const ndx = crossfilter(this.recipeData[3]);
+
+        const course_dim = ndx.dimension(dc.pluck('CourseName'));
+        const cuisine_dim = ndx.dimension(dc.pluck('CuisineName'));
+        const total_course = course_dim.group();
+        const total_cuisine = cuisine_dim.group();
+
+        dc.pieChart('#cuisine-chart')
+            .height(200)
+            .radius(120)
+            .transitionDuration(1500)
+            .dimension(cuisine_dim)
+            .group(total_cuisine);
+
+        dc.pieChart('#course-chart')
+            .height(200)
+            .radius(120)
+            .transitionDuration(1500)
+            .dimension(course_dim)
+            .group(total_course);
+        
+
+        dc.renderAll();
+    };
+}
+
+
+//--------------------------- XHR ----------------------------------------//
+
+class AJAXQuery { 
+
+    constructor(url){
+
+        this.url = url;
+    }
+
+    getRequest() {
+
+        return new Promise((resolve, reject) => {
+
+            const xhr = new XMLHttpRequest();
+
+            xhr.open("GET", this.url, true)
+            xhr.onload = function() {
+        
+                if (xhr.readyState == 4 && xhr.status == 200) { 
+                    const response = xhr.responseText;
+                    resolve(response)
+                
+                } else {
+                    const error = xhr.responseText;
+                    reject(error)
+                }
+            };
+            
+            xhr.send()  
+        });
+    };
+
+    getRecipesData() {
+
+        // RETRIEVE WORD FROM SERVER AS JSON //
+        this.getRequest()
+            .then((response) => { 
+
+                const data = JSON.parse(response);
+                const recipeData = data;
+                const chart = new Charts(recipeData);
+                chart.makePieGraph();      
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+    };
+
+}
+    
+    //---------------------------------------------------------------------------//
+    //---------------------------------------------------------------------------//
+
+    //------------------------ BUTTON AND FORM VARIABLES ----------------------------------//
+class LoginVariables {
+
+    constructor() {
+
+        this.signUpButton = document.getElementById('signup-button');
+        this.logInButton = document.getElementById('login-button');
+        this.signUpForm = document.getElementById('signup');
+        this.logInForm = document.getElementById('login');
+
+        // ACCORDIAN VARIABLES FOR JOIN US AND MORE INFO BUTTON //
+        this.joinUsButton = document.getElementById('join-us-button');
+        this.formsContainer = document.getElementById('forms-container');
+        this.stingContainer = document.getElementById('sting');
+
+        this.moreInfoButton = document.getElementById('more-info');
+        this.moreInfoContainer = document.getElementById('more-info-container');
+        this.scrollToStat = document.getElementById('stat-scroll');
+        this.statsContainer = document.getElementById('stats');
+
+        // STICKY HEADER VARIABLES //
+        this.header = document.getElementById('header');
+        this.unStick = document.getElementById('stats').offsetTop;
+    }
+
+    getVariables() {
+
+        return this.signUpButton,this.logInButton,this.signUpForm,
+        this.logInForm, this.joinUsButton,  this.formsContainer, this.stingContainer,
+        this.moreInfoButton, this.moreInfoContainer, this.scrollToStat, this.statsContainer,
+        this.header, this.sticky;
+        
+    }
+    }
+    //-------------------------- ACCORDIANS ----------------------------------------//
+
+class FormAccordian {
+    // DROP DOWN FORM AND CLOSE ALREADY OPENED FORM //
+    formAccordian(button, show,  showStyle){
+
+        button.addEventListener('click', () => {
+
+            show.classList.toggle(`${showStyle}`);
+            
+        });
+        
+    };
+
+    // SIGN UP FORM ACCORDDIAN SECTION //
+    formSectionAccordian(button, formsContainer, stingContainer) {
+        button.addEventListener('click', () => {
+
+            formsContainer.classList.toggle('forms-login-display');
+            stingContainer.classList.toggle('sting--style');
+        
+        });
+
+    }
+}
+
+class StatAccordian {
+
+    constructor(moreInfoButton, statsContainer, moreInfoContainer) { 
+
+        this.moreInfoButton = moreInfoButton;
+        this.statsContainer = statsContainer;
+        this.moreInfoContainer = moreInfoContainer;
+    
+    }
+    // STATS SECTION ACCORDIAN WITH SCROLL TO FUNCTIONALITY //
+    statsAccordian(){
+
+        this.moreInfoButton.addEventListener('click', () => {
+            
+            if (this.statsContainer.classList.contains('stats-show')) {
+
+                //scrollTo(this.moreInfoContainer);
+                this.statsContainer.classList.remove('stats-show');
+                this.moreInfoContainer.classList.remove('more-info--style');
+
+            } else {
+            // getRecipesData('/stats');
+                this.statsContainer.classList.add('stats-show');
+                this.moreInfoContainer.classList.add('more-info--style');
+                setTimeout(this.scrollTo, 1000);
+            }
+            
+        });
+    }
+
+    //------------------------------ AUTO SCROLL -----------------------------//
+
+    // SCROLL TO WINDOW ELEMENT FUNCTION // 
+    scrollTo() {
+        
+        console.log('here');
+        window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: document.getElementById('stat-scroll').offsetTop
+        });
+    }
+
+}
+
+
+//------------------------ STICKY HEADER -------------------------------//
+
+const stickyHeader = (header, unStick) => {
+
+    if (window.pageYOffset <= unStick){
+
+        header.classList.remove('sticky');
+
+    } else {
+
+        header.classList.add('sticky');
+    }
+}
+
+//-------------------------------------------------------------------------//
+
+//------------------------- CLICK STYLES ---------------------------------//
+
+const formButtonClick = (button, styles) => {
+
+    button.addEventListener('mousedown', () => {
+
+        button.classList.add(`${styles}`);
+    });
+
+    button.addEventListener('mouseup', () => {
+
+        button.classList.remove(`${styles}`);
+    });
+}
+    
+
--- static/js/scripts/login.js
+++ static/js/scripts/login.js
@@ -1,226 +1,31 @@
-//------------------------- FUNCTION DECLARATIONS ------------------------//
+// ----------------- LOGIN PAGE ---------------------------------------------------- //
 
-//--------------------------- XHR EXPRESSIONS ----------------------------------------//
+const element = new LoginVariables();
+const form = new FormAccordian();
+const stat = new StatAccordian(element.moreInfoButton, element.statsContainer, 
+                                element.moreInfoContainer);
 
-const returnData = (data) => {
-
-    return data;
-};
-
-const getRequest = (url) => {
-
-    return new Promise((resolve, reject) => {
-
-        const xhr = new XMLHttpRequest();
-
-        xhr.open("GET", url, true)
-        xhr.onload = function() {
-    
-            if (xhr.readyState == 4 && xhr.status == 200) { 
-
-                const response = xhr.responseText;
-                resolve(response)
-            
-            } else {
-                const error = xhr.responseText;
-                reject(error)
-            }
-        };
-        
-        xhr.send()  
-    });
-};
-
-
-// -------------------------------- VISUALISATION ---------------------------- //
-
-const makePieGraph = (recipeData) => {
-
-    const ndx = crossfilter(recipeData[3]);
-
-    const course_dim = ndx.dimension(dc.pluck('CourseName'));
-    const cuisine_dim = ndx.dimension(dc.pluck('CuisineName'));
-    const total_course = course_dim.group();
-    const total_cuisine = cuisine_dim.group();
-
-    dc.pieChart('#cuisine-chart')
-        .height(200)
-        .radius(120)
-        .transitionDuration(1500)
-        .dimension(cuisine_dim)
-        .group(total_cuisine);
-
-    dc.pieChart('#course-chart')
-        .height(200)
-        .radius(120)
-        .transitionDuration(1500)
-        .dimension(course_dim)
-        .group(total_course);
-       
-
-    dc.renderAll();
-};
-
-
-const getRecipesData = (url) => {
-
-    // RETRIEVE WORD FROM SERVER AS JSON //
-    getRequest(url)
-        .then((response) => { 
-
-            const data = JSON.parse(response);
-            const recipeData = data;
-            makePieGraph(recipeData);
-            //console.log(recipeData);
-            
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-
-};
-
-//---------------------------------------------------------------------------//
-//---------------------------------------------------------------------------//
-
-//------------------------ BUTTON AND FORM VARIABLES ----------------------------------//
-
-const signUpButton = document.getElementById('signup-button');
-const logInButton = document.getElementById('login-button');
-const signUpForm = document.getElementById('signup');
-const logInForm = document.getElementById('login');
-
-// ACCORDIAN VARIABLES FOR JOIN US AND MORE INFO BUTTON //
-const joinUsButton = document.getElementById('join-us-button');
-const formsContainer = document.getElementById('forms-container');
-const stingContainer = document.getElementById('sting');
-
-const moreInfoButton = document.getElementById('more-info');
-const moreInfoContainer = document.getElementById('more-info-container');
-const scrollToStat = document.getElementById('stat-scroll');
-const statsContainer = document.getElementById('stats');
-
-//-------------------------- ACCORDIAN FUNCTIONS ----------------------------------------//
-
-// DROP DOWN FORM AND CLOSE ALREADY OPENED FORM //
-const formAccordian = (button, show,  showStyle) => {
-
-    button.addEventListener('click', () => {
-
-        show.classList.toggle(`${showStyle}`);
-        
-    });
-    
-};
-
-// SIGN UP FORM ACCORDDIAN SECTION //
-const formSectionAccordian = () => {
-
-    joinUsButton.addEventListener('click', () => {
-
-        formsContainer.classList.toggle('forms-login-display');
-        stingContainer.classList.toggle('sting--style');
-    
-    });
-
-}
-
-// SCROLL TO WINDOW ELEMENT FUNCTION // 
-
-const scrollTo = (element) => {
-    window.scroll({
-      behavior: 'smooth',
-      left: 0,
-      top: element.offsetTop
-    });
-  }
-
-// STATS SECTION ACCORDIAN WITH SCROLL TO FUNCTIONALITY //
-
-const statsAccordian = () => {
-
-    moreInfoButton.addEventListener('click', () => {
-        
-        if (statsContainer.classList.contains('stats-show')) {
-
-            scrollTo(moreInfoContainer);
-            statsContainer.classList.remove('stats-show');
-            moreInfoContainer.classList.remove('more-info--style');
-
-        } else {
-           // getRecipesData('/stats');
-            statsContainer.classList.add('stats-show');
-            moreInfoContainer.classList.add('more-info--style');
-            setTimeout(scrollToStatElement, 500);
-        }
-        
-    });
-}
-
-//------------------------------ AUTO SCROLL -----------------------------//
-
-// CALL SCROLL TO FUNCTION FOR ELEMENT //
-const scrollToStatElement = () => {
-
-    if (statsContainer.classList.contains('stats-show')) {
-
-        scrollTo(scrollToStat);
-    }
-
-}
-
-//------------------------ STICKY HEADER -------------------------------//
-
-const header = document.getElementById('header');
-const sticky = statsContainer.offsetTop;
-
-const stickyHeader = () => {
-
-    if (window.pageYOffset <= sticky){
-
-        header.classList.remove('sticky');
-
-    } else {
-
-        header.classList.add('sticky');
-    }
-}
-
-//-------------------------------------------------------------------------//
-
-//------------------------- CLICK STYLES ---------------------------------//
-
-const formButtonClick = (button, styles) => {
-
-    button.addEventListener('mousedown', () => {
-
-        button.classList.add(`${styles}`);
-    });
-
-    button.addEventListener('mouseup', () => {
-
-        button.classList.remove(`${styles}`);
-    });
-}
-
-// ----------------------- FUNCTION CALLS ------------------------------- //
-
+//-------------------------- STICKY HEADER --------------------------------------//
 window.onscroll = () => {
 
-    stickyHeader();
+   stickyHeader(element.header, element.unStick);
 }
 
-// ACCORDIANS //
+// CALL ACCORDIANS //
 
-formSectionAccordian();
-statsAccordian();
-formAccordian(joinUsButton, signUpForm, 'show-signup-form');
+form.formSectionAccordian(element.joinUsButton, element.formsContainer, 
+                                                element.stingContainer);
+stat.statsAccordian();
+form.formAccordian(element.joinUsButton, element.signUpForm, 'show-signup-form');
 
-//CALL XHR //
+// CALL XHR //
 
-getRecipesData('/stats');
+const query = new AJAXQuery('/stats');
+query.getRecipesData();
 
 // CLICK STYLES //
 
-formButtonClick(logInButton, 'login__button--click');
-formButtonClick(signUpButton, 'signup__button--click');
+formButtonClick(element.logInButton, 'login__button--click');
+formButtonClick(element.signUpButton, 'signup__button--click');
+
+//------------------------------------------------------------------------------------//
