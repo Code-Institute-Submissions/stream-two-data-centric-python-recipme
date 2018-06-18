2835c9269ffe9636966f25c78362780b3f73bdc4
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-06-18 16:13:24 +0100

    secret key hidden

--- recipme_app.py
+++ recipme_app.py
@@ -370,10 +370,8 @@ if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
 
 """
-##PORT = int(os.environ.get("PORT", 5000))
-##IP = '127.0'
-
+PORT = int(os.environ.get("PORT", 5000))
 
 if __name__ == '__main__':
-    app.run(debug=False, host=os.environ.get('IP'), port = os.environ.get('PORT'))
+    app.run(debug=False, host=os.environ.get('IP'), port = PORT)
 
